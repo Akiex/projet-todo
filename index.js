1026eb1d8f8c98d446ed898e38722d52bad2c7c9
Tandé Hugo <htande@hotmail.fr>
2024-12-02 09:54:51 +0000

    sessionStorage

--- index.js
+++ index.js
@@ -14,19 +14,33 @@ info().then(data => {
     
     
     for(let i = 0; i<data.todos.length; i++){
-        let li = document.createElement('li')
-        li.class = 'task'
-        let check = document.createElement('input')
-        check.type = 'checkbox'
-        check.class = 'checkBox'
-        let info = data.todos[i]
-        li.textContent = info.todo
-        li.appendChild(check)
-        ul.appendChild(li)
+        let li = document.createElement('li');
+        li.classList.add = ('task');
+        
+        let check = document.createElement('input');
+        check.type = 'checkbox';
+        check.classList.add = ('checkBox');
+        
+        let info = data.todos[i];
+        
+        li.textContent = info.todo;
+        li.appendChild(check);
+        ul.appendChild(li);
+        
+        check.addEventListener('change', function() {
+           
+            window.localStorage.setItem(`check-${info.id}`, check.checked);
+        });
+        
+        
+        const storedChecked = window.localStorage.getItem(`check-${info.id}`);
+        if (storedChecked === 'true') {
+            check.checked = true;
+        }
+    
     }
 }).catch(error => {
     console.log('Erreur lors de la création de : ', error);
 })
 
 
-window.localStorage.setItem('check', true, false);
