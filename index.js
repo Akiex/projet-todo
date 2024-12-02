827e6d16ea1214cc50916386072d17176d71720e
Joris Guesné <joris.dev@proton.me>
2024-12-02 12:07:32 +0100

    Ajout du CSS

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-let url = "https://dummyjson.com/todos?limit=12";
-
 async function info() {
   let url = "https://dummyjson.com/todos?limit=12";
   let response = await fetch(url);
-
-  // JE FAIS UN TEST
-
-  // Je fais un 2ème test
   let data = await response.json();
 
   return data;
 }
+info()
+  .then((data) => {
+    console.log(data);
+
+    let ul = document.querySelector("#ToDoList");
+
+    for (let i = 0; i < data.todos.length; i++) {
+      let li = document.createElement("li");
+      li.classList.add = "task";
+
+      let check = document.createElement("input");
+      check.type = "checkbox";
+      check.classList.add = "checkBox";
+
+      let info = data.todos[i];
+
+      li.textContent = info.todo;
+      li.appendChild(check);
+      ul.appendChild(li);
+
+      check.addEventListener("change", function () {
+        window.localStorage.setItem(`check-${info.id}`, check.checked);
+      });
+
+      const storedChecked = window.localStorage.getItem(`check-${info.id}`);
+      if (storedChecked === "true") {
+        check.checked = true;
+      }
+    }
+  })
+  .catch((error) => {
+    console.log("Erreur lors de la création de : ", error);
+  });
